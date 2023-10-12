c22be672a2b6182438895c71f6b2fd38bd11698f
Jorata <georgi.tomov.popov@outlook.com>
2023-10-12 10:23:19 +0300

    Adding P1OpinionPoll

---
+++ src/P1OpinionPoll.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class P1OpinionPoll {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        List<Person> people = new ArrayList<>();
+
+        while (n-- > 0) {
+            String[] input = scanner.nextLine().split("\\s+");
+            String name = input[0];
+            int age = Integer.parseInt(input[1]);
+
+            if (age > 30) {
+                Person person = new Person(name, age);
+                people.add(person);
+            }
+        }
+
+        people.stream().sorted(Comparator.comparing(Person::getName))
+                .forEach(System.out::println);
+
+
+    }
+}
